d1f77797561c3229dda8afd0a304684d53460528
MontanaRoberts <montanawroberts@gmail.com>
2017-11-16 17:46:30 -0800

    translated parsing into ruby

--- script_editor/app/controllers/plays_controller.rb
+++ script_editor/app/controllers/plays_controller.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
+require "create_scripts"
 
 class PlaysController < ApplicationController
   
@@ -21,75 +22,9 @@ class PlaysController < ApplicationController
       @provideddocument = File.open(@string, "r")
       @provideddocument = File.read(@string)
     else 
+      print "NO DOCUMENT PROVIDED. CREATING NEW ONE"
       @provideddocument = ""  #perhaps here we can provide the html files we already have?
       #until then we can check in the HTML if this variable is equal to "" and if so parse it there
     end
-    
-    # These variables will change as we parse through the play
-    #theSynopsis = doc.css('//div[type="synopsis"]').text
-    #currentPlay = doc.css('//titleStmt/title').inner_text
-    #currentLine = "default"
-    #currentWord = "default"
-    #wordId = "default"
-    #currentSpeaker = "default"
-    #speakerNameHasTwoWords = false
-    #wordIsSpeaker = false
-    #oneMoreAct = false
-    #oneMoreScene = false
-
-    # Add the title to the top of the page
-    #title = Nokogiri::XML::Node.new "p", htmldoc
-    #title['id'] = 'title'
-    #title.content = currentPlay
-    #titleP.add_next_sibling(title)
-
-    # Add the synopsis text but hide it
-    #hidden = Nokogiri::XML::Node.new "div", htmldoc
-    #hidden['class'] = 'hidden-synopsis'
-    #hidden.content = theSynopsis
-    #navP.add_next_sibling(hidden)
-
-    # Add a tab for synopsis in navigation bar
-    #synopsis = Nokogiri::XML::Node.new "button", htmldoc
-    #synopsis['class'] = 'nav-synopsis'
-    #synopsis.content = "SYNOPSIS"
-    #synopsis.parent = div
-    #currentDiv = synopsis
-
-    # Loop through all of the words in the play
-  	#doc.css('w').each do |node|
-  		#children = node.children
-
-      #currentLine = node['n']
-      #wordId = node['xml:id']
-      #currentWord = children.inner_text
-
-      # This loops checks if the word is a speaker and updates currentSpeaker
-  		#if (currentWord == currentWord.upcase)
-  			#if (currentWord.length > 2 and not(currentWord == "ACT") and not(currentWord == "EPILOGUE"))
-  				#if (speakerNameHasTwoWords)
-            #currentSpeaker += " " + currentWord
-            #speakerNameHasTwoWords = false
-          #elsif (currentWord == "FIRST" or currentWord == "SECOND" or currentWord == "THIRD")
-            #currentSpeaker = currentWord
-            #speakerNameHasTwoWords = true
-          #else 
-            #currentSpeaker = currentWord
-          #end
-          #wordIsSpeaker = true
-        #else
-          #wordIsSpeaker = false
-  			#end
-      #else
-        #wordIsSpeaker = false
-      #end
-
-    
-
-
-
-  	#end	
-    #puts htmldoc
-    #File.write("app/views/plays/show.html.erb", htmldoc)
   end
 end
